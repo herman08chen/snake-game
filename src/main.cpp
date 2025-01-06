ab8b66009878704cbadd2c48b164bbb37fd9aa1c
herman08chen <herman08chen@gmail.com>
2025-01-05 21:30:47 -0500

    added gameover

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,12 @@
 #include <random>
 #include <bit>
 #include <format>
+#include <iostream>
 
 enum axis { POSX = 1, POSY = 2, POSZ = 3, NEGX = -1, NEGY = -2, NEGZ = -3};
+
+static constexpr int SIDE = 12;
+static constexpr int HSIDE = 6;
 
 struct point {
     int x; 
@@ -24,9 +28,9 @@ struct point {
 
 void draw_grid(const auto& coins) {
     static constexpr point cubePosition {0, 0, 0};
-    static constexpr std::size_t side_length = 12;
+    static constexpr std::size_t side_length = SIDE;
     DrawCubeWires(cubePosition, side_length, side_length, side_length, MAROON);
-    DrawGrid(12, 1.0f);
+    DrawGrid(SIDE, 1.0f);
 }
 
 void draw_cube(const point& pos, const float& side_length = 1.0f, const Color& color = RED) {
@@ -39,13 +43,14 @@ void draw_cube(const point& pos, const float& side_length = 1.0f, const Color& c
 struct snake {
     std::list<point> cubes {point{}, point{ -1, 0, 0 }, point{ -2, 0, 0 }};
     axis direction = POSX, prev_direction = POSX;
+    bool collided = false;
 
     point& head() {
         return cubes.front();
     }
 
     void increase_length() {
-        cubes.emplace_back(0, 0, 0);
+        cubes.emplace_back(-1000, -1000, -1000);
     }
 
     void update_direction() {
@@ -94,10 +99,13 @@ struct snake {
             head().z--;
             break;
         }
-        head().x = std::clamp<int>(head().x, -6, 6);
-        head().y = std::clamp<int>(head().y, -6, 6);
-        head().z = std::clamp<int>(head().z, -6, 6);
+
         prev_direction = direction;
+
+        if (head().x < -HSIDE || head().x > HSIDE) collided = true;
+        if (head().y < -HSIDE || head().y > HSIDE) collided = true;
+        if (head().z < -HSIDE || head().z > HSIDE) collided = true;
+        if (std::ranges::contains(cubes | std::ranges::views::drop(1), head()))  collided = true;
     }
     void draw() {
         for (auto&& cube : cubes | std::ranges::views::drop(1)) {
@@ -110,7 +118,7 @@ struct snake {
 point rand_point() {
     std::random_device rd;  
     std::mt19937 gen(rd()); 
-    std::uniform_int_distribution<> distrib(-8, 8);
+    std::uniform_int_distribution<> distrib(-HSIDE, HSIDE);
     return point{ distrib(gen), distrib(gen), distrib(gen) };
 }
 
@@ -139,27 +147,31 @@ struct game {
 
     std::size_t score{};
 
+    bool game_over = false;
+
     void update() {
         frame_count++;
         my_snake.update_direction();
         if (frame_count % 24 == 0) {
+            my_snake.move();
+            if (my_snake.collided) {
+                game_over = true;
+                return;
+            }
             auto it = coins.find(my_snake.head());
             if (it != coins.end()) {
                 my_snake.increase_length();
                 coins.erase(it);
                 score++;
             }
-            my_snake.move();
+            
         }
-        if (frame_count % 128 == 0 && coins.size() != 5) {
+        if (frame_count % 128 == 0 && coins.size() != 5 && !game_over) {
             auto coin = rand_point();
             if (!std::ranges::contains(my_snake.cubes, coin)) coins.insert(coin);
         }
     }
 
-    void draw() {
-        
-    }
 };
 
 int main(void)
@@ -183,26 +195,35 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())
     {
-        GAME.update();
+        
+        if(!GAME.game_over) GAME.update();
 
-        BeginDrawing();
+        if (GAME.game_over) {
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Game Over!", 30, 30, 32, BLACK);
+            DrawText(std::format("Score: {}", GAME.score).c_str(), 30, 70, 32, BLACK);
 
-        ClearBackground(RAYWHITE);
+            EndDrawing();
+        }
+        else {
+            BeginDrawing();
 
-        DrawText(std::format("Score: {}", GAME.score).c_str(), 30, 30, 32, BLACK);
+            ClearBackground(RAYWHITE);
 
-        BeginMode3D(camera);
+            DrawText(std::format("Score: {}", GAME.score).c_str(), 30, 30, 32, BLACK);
 
-        draw_grid(GAME.grid);
+            BeginMode3D(camera);
 
-        GAME.my_snake.draw();
-        draw_coins(GAME.coins);
+            draw_grid(GAME.grid);
 
-        EndMode3D();
+            GAME.my_snake.draw();
+            draw_coins(GAME.coins);
 
-        //DrawFPS(10, 10);
+            EndMode3D();
 
-        EndDrawing();
+            EndDrawing();
+        }
     }
 
     CloseWindow();       
